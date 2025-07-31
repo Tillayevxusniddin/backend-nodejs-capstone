1145be761480e746e710803cd6083e8479fb86a3
Tillayevxusniddin <tillayevx1@gmail.com>
2025-07-31 16:41:17 +0500

    refactoring two

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -4,8 +4,7 @@ const cors = require('cors');
 const path = require('path');
 const pinoLogger = require('./logger');
 const connectToDatabase = require('./models/db');
-const { loadData } = require("./util/import-mongo/index");
-const pinoHttp = require('pino-http');
+const { loadData } = require('./util/import-mongo/index'); 
 const logger = require('./logger');
 
 const secondChanceItemsRoutes = require('./routes/secondChanceItemsRoutes');
@@ -13,11 +12,11 @@ const authRoutes = require('./routes/authRoutes');
 const searchRoutes = require('./routes/searchRoutes');
 
 const app = express();
-app.use("*", cors());
+app.use('*', cors());
 const port = 3060;
 
 connectToDatabase().then(() => {
-    pinoLogger.info('Connected to DB');
+  pinoLogger.info('Connected to DB'); 
 }).catch((e) => console.error('Failed to connect to DB', e));
 
 app.use(pinoHttp({ logger }));
@@ -30,19 +29,19 @@ app.use('/api/secondchance/items', secondChanceItemsRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/secondchance/search', searchRoutes);
 
-app.get("/", (req, res) => {
-    res.send("Inside the server");
+app.get('/', (req, res) => {
+  res.send('Inside the server'); 
 });
 
 app.use((err, req, res, next) => {
-    console.error(err);
-    logger.error(err.stack);
-    res.status(500).send('Internal Server Error');
+  console.error(err);
+  logger.error(err.stack);
+  res.status(500).send('Internal Server Error'); 
 });
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-    pinoLogger.info(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`); 
+  pinoLogger.info(`Server running on port ${port}`); 
 });
 
 module.exports = app;
